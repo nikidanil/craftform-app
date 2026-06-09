3133278c49e77cfa46ce37c5129419e89c3ab841
Nikita <nekit9401@yandex.ru>
2026-06-09 10:38:46 +0300

    refactor: inline QueryClientProvider, drop AppProviders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router/dom';
-import { AppProviders } from './providers/AppProviders';
+import { queryClient } from './providers/queryClient';
 import { router } from './router/router';
 
 export const App = () => (
-	<AppProviders>
+	<QueryClientProvider client={queryClient}>
 		<RouterProvider router={router} />
-	</AppProviders>
+	</QueryClientProvider>
 );
