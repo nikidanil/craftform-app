bae95e1718328b18f7fd036255718d2bb6ad4328
Nikita <nekit9401@yandex.ru>
2026-06-09 10:38:46 +0300

    refactor: inline QueryClientProvider, drop AppProviders

--- src/app/router/RootLayout.tsx
+++ src/app/router/RootLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router';
 import { ScrollToTop } from './ScrollToTop';
+import { BackgroundBlobs } from '../ui/BackgroundBlobs';
 
 export const RootLayout = () => (
 	<>
+		<BackgroundBlobs />
 		<ScrollToTop />
 		<Outlet />
 	</>
